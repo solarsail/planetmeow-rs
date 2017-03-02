124693028d6bb5c2674adfcb2f0e2c6148bc9617
dev <dev@localhost.localdomain>
2017-03-02 14:22:14 +0800

    added db functions

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,16 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate diesel_codegen;
+extern crate dotenv;
+extern crate time;
 
 mod handlers;
+mod db;
+mod models;
+mod schema;
+
 
 #[get("/")]
 fn index() -> &'static str {
